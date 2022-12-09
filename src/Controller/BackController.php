ce838996e8c2d6d22a9541ce910cd1c277865d34
MrKyroshi <mrkyroshi@gmail.com>
2022-12-09 09:47:01 +0100

    neuw 9/12

--- src/Controller/BackController.php
+++ src/Controller/BackController.php
@@ -2,30 +2,131 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\EditProductType;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BackController extends AbstractController
 {
-       #[Route('/ajoutProduit', name: 'ajoutProduit')]
-           public function ajoutProduit(): Response
+    #[Route('/ajoutProduit', name: 'ajoutProduit')]
+    public function ajoutProduit(Request $request, EntityManagerInterface $manager): Response
+    {
+        // cette methode nous permettre de créer un nouveau produit .on instancie donc un objet Produt de App/Entity que l'on va remplir de toutes se propriétés
+        $product = new Product();
+
+        //Ici on instancie un objet Form via la méthode createForm() existance de notre abstractController
+        $form = $this->createForm(ProductType::class, $product);
+        //cette methode attend en argument le formulaire à utiliser et l'objet Entité auquel il fait référence , ainsi il va controller la conformité entre les champs formulaire et les propriétés présentes dans l'entité pour pouvoir remplir l'objet Product par lui-même.
+
+        //grace à la méthode handLeRequest de notre objet de formulaire, il charge à présent l'objet Product de données receptionnées du formulaire présentent dans notre objet request (Request étant la classe de symfony qui récupère la majeur partie des données de superGLOBALE=>$_GET,$_POST.......)
+        $form->handleRequest($request);
+
+         //$request->request est la surcouche de $_POST.->get() permet d'accéder à une entrée de notre tableau de donnée
+        //$request->request->get('title');
+
+        // pour accéder à la surcouche de $_GET on utilise $request->query
+        // qui possède les mêmes méthodes que $request->request
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            //dd($product);
+            //dd($form->get('picture')->getData());
+            // on récupère les donnes de notre inout type File du formulaire qui a pour name 'picture'
+            $picture=$form->get('picture')->getData();
+            // condition d'upload de photo
+            if($picture){
+                $picture_bdd=date('YmdHis').uniqid().$picture->getClientOriginalName();
+
+                $picture->move ($this->getParameter('upload_directory'),$picture_bdd);
+                //move() est une méthode de notre objet File qui permet de déplacer notre fichier temporaire uploadé à une emplacement donné( le 1er paramètre) et de nommé ce fichier ( le second paramètre de la méthode)
+
+                $product->setPicture($picture_bdd);
+                $manager->persist($product);
+                $manager->flush();
+
+                $this->addFlash('success','Produit ajouté ! ');
+                return $this->redirectToRoute('gestionProduit');
+
+
+
+            }
+
+
+        }
+
+        return $this->render('back/ajoutProduit.html.twig', [
+            'form'=>$form->createView()
+
+
+        ]);
+    }
+       #[Route('/gestionProduit', name: 'gestionProduit')]
+           public function gestionProduit(ProductRepository $productRepository): Response
            {
-               return $this->render('back/ajoutProduit.html.twig', [
-                  
+               $products=$productRepository->findAll();
+
+
+               return $this->render('back/gestionProduit.html.twig', [
+                  'products'=>$products
                ]);
            }
     
+           
+              #[Route('/editProduct/{id}', name: 'editProduct')]
+                  public function editProduct(Product $product, Request $request,EntityManagerInterface $manager): Response
+                  {
+
+                      $form=$this->createForm(EditProductType::class,$product);
+
+                      $form->handleRequest($request);
+
+                      if ($form->isSubmitted() && $form->isValid())
+                      {
+                          if($form->get('editPicture')->getData()){
+                           $picture=$form->get('editPicture')->getData();
+                              $picture_bdd=date('YmdHis').uniqid().$picture->getClientOriginalName();
+
+                              $picture->move ($this->getParameter('upload_directory'),$picture_bdd);
+                              unlink($this->getParameter('upload_directory').'/'.$product->getPicture());
+                              $product->setPicture($picture_bdd);
+
+
+                          }
+                          $manager->persist($product);
+                          $manager->flush();
+
+                          $this->addFlash('success','Produit modifié');
+                          return $this->redirectToRoute('gestionProduit');
+
+                      }
+
+                      return $this->render('back/editProduct.html.twig', [
+                         'form'=>$form->createView(),
+                          'product'=>$product
+                      ]);
+                  }
+
+
+
+                     #[Route('/deleteProduct/{id}', name: 'deleteProduct')]
+                         public function deleteProduct(Product $product,EntityManagerInterface $manager): Response
+                         {
+                             $manager->remove($product);
+                             $manager->flush();
+
+
+                             $this->addFlash('success','produit supprimé !!! ');
+
+                             return $this->redirectToRoute('gestionProduit');
+                         }
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-}
+
+}//fermeture de controller
--- src/Controller/FontController.php
+++ src/Controller/FontController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class FontController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function home(): Response
+    public function home(ProductRepository $productRepository): Response
     {
-        return $this->render('front/home.html.twig', [
+        $products=$productRepository->findAll();
 
+
+        return $this->render('front/home.html.twig', [
+           'products'=>$products
         ]);
     }
 }
