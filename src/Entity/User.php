8c5539feed3e7f16b03f9ab5822839b849e78cc1
MrKyroshi <mrkyroshi@gmail.com>
2022-12-09 10:04:59 +0100

    authentification

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,9 +5,13 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+#[UniqueEntity(fields: ['email'],message: 'Un compte existe déjà !!!')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -30,7 +34,7 @@ class User
     private ?string $tel = null;
 
     #[ORM\Column(type: Types::ARRAY)]
-    private array $roles = [];
+    private array $roles = ['ROLE_USER'];
 
     public function getId(): ?int
     {
@@ -107,5 +111,15 @@ class User
         $this->roles = $roles;
 
         return $this;
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->email;
     }
 }
